c505541b3a54417295f3e862fed6545ad09252e9
IvanNepo <156950275+IvanNepo@users.noreply.github.com>
2024-01-31 19:01:53 +0300

    Add files via upload

--- HW_l4_3/Program.cs
+++ HW_l4_3/Program.cs
@@ -7,14 +7,11 @@
             Console.WriteLine("Enter a positive integer: ");
             int number = int.Parse(Console.ReadLine());
             int sum = 0;
-            int i = 1;
-            while (i <= number)
+            for (int i = 1;i <= number ; i++) 
             {
-                sum += i;
-                i++;
+            sum += i;
             }
             Console.WriteLine("Sum of all numbers from 1 to {0} equals {1}");
-            Console.WriteLine();
         }
     }
 }
